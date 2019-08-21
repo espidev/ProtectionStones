e8da23d3439103717f2fbc95d16817870dc51171
EspiDev <espidev@gmail.com>
2019-08-21 18:47:34 -0400

    Region traversal now loops with state instead of recursion

--- src/main/java/dev/espi/protectionstones/utils/RegionTraverse.java
+++ src/main/java/dev/espi/protectionstones/utils/RegionTraverse.java
@@ -21,6 +21,7 @@ import com.sk89q.worldedit.math.BlockVector2;
 import com.sk89q.worldedit.math.Vector2;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import dev.espi.protectionstones.ProtectionStones;
+import jdk.nashorn.internal.runtime.regexp.joni.constants.Traverse;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -46,27 +47,113 @@ public class RegionTraverse {
     }
 
     private static class TraverseData {
-        BlockVector2 v, previous, start;
+        BlockVector2 v, previous;
         boolean first;
 
+        TraverseData(BlockVector2 v, BlockVector2 previous, boolean first) {
+            this.v = v;
+            this.previous = previous;
+            this.first = first;
+        }
     }
 
     private static boolean isInRegion(BlockVector2 point, List<ProtectedRegion> regions) {
-        for (ProtectedRegion r : regions) {
+        for (ProtectedRegion r : regions)
             if (r.contains(point)) return true;
-        }
         return false;
     }
 
+
+    // can't use recursion because stack overflow
     public static void traverseRegionEdge(HashSet<BlockVector2> points, List<ProtectedRegion> regions, Consumer<TraverseReturn> run) {
         int pointID = 0;
         while (!points.isEmpty()) {
             BlockVector2 start = points.iterator().next();
-            traverseRegionEdge(start, null, start, true, points, regions, pointID, run);
+            TraverseData td = new TraverseData(start, null, true);
+            //traverseRegionEdge(start, null, start, true, points, regions, pointID, run);
 
-            // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ algorithm starts ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+            boolean cont = true;
+            while (cont) {
+                cont = false;
+                BlockVector2 v = td.v, previous = td.previous;
 
+                if (!td.first && v.equals(start)) break;
+
+                int exposedEdges = 0;
+                List<BlockVector2> insideVertex = new ArrayList<>();
+                for (Vector2 dir : DIRECTIONS) {
+                    BlockVector2 test = BlockVector2.at(v.getX() + dir.getX(), v.getZ() + dir.getZ());
+                    if (!isInRegion(test, regions)) {
+                        exposedEdges++;
+                    } else {
+                        insideVertex.add(test);
+                    }
+                }
+                points.remove(v); // remove current point if it exists
+
+                switch (exposedEdges) {
+                    case 1: // normal edge
+                        run.accept(new TraverseReturn(v, false, pointID, exposedEdges)); // run consumer
+
+                        if (previous == null) { // if this is the first node we need to determine a direction to go to (that isn't into the polygon, but is on edge)
+                            if (insideVertex.get(0).getX() == insideVertex.get(1).getZ() || insideVertex.get(0).getZ() == insideVertex.get(1).getZ() || insideVertex.get(0).getX() == insideVertex.get(2).getZ() || insideVertex.get(0).getZ() == insideVertex.get(2).getZ()) {
+                                previous = insideVertex.get(0);
+                            } else {
+                                previous = insideVertex.get(1);
+                            }
+                        }
+                        td = new TraverseData(BlockVector2.at(v.getX() + (v.getX() - previous.getX()), v.getZ() + (v.getZ() - previous.getZ())), v, false);
+                        cont = true;
+                        break;
+                    case 2: // convex vertex
+                        // possibly also 1 block wide segment with 2 edges opposite, but we'll ignore that
+                        run.accept(new TraverseReturn(v, true, pointID, exposedEdges)); // run consumer
+                        if (insideVertex.get(0).equals(previous)) {
+                            td = new TraverseData(insideVertex.get(1), v, false);
+                            cont = true;
+                        } else {
+                            td = new TraverseData(insideVertex.get(0), v, false);
+                            cont = true;
+                        }
+                        break;
+                    case 3: // random 1x1 jutting out
+                        if (isInRegion(v, regions)) ProtectionStones.getInstance().getLogger().info("Reached impossible situation in region edge traversal at " + v.getX() + " " + v.getZ() + ", please notify the developers that you saw this message!");
+                        break;
+                    case 0: // concave vertex, or point in middle of region
+                        List<Vector2> cornersNotIn = new ArrayList<>();
+                        for (Vector2 dir : CORNER_DIRECTIONS) {
+                            BlockVector2 test = BlockVector2.at(v.getX() + dir.getX(), v.getZ() + dir.getZ());
+
+                            if (!isInRegion(test, regions)) cornersNotIn.add(dir);
+                        }
+
+                        if (cornersNotIn.size() == 1) { // concave vertex
+                            run.accept(new TraverseReturn(v, true, pointID, exposedEdges)); // run consumer
+
+                            Vector2 dir = cornersNotIn.get(0);
+                            if (previous == null || previous.equals(BlockVector2.at(v.getX() + dir.getX(), v.getZ()))) {
+                                td = new TraverseData(BlockVector2.at(v.getX(), v.getZ() + dir.getZ()), v, false);
+                                cont = true;
+                            } else {
+                                td = new TraverseData(BlockVector2.at(v.getX() + dir.getX(), v.getZ()), v, false);
+                                cont = true;
+                            }
+                        } else if (cornersNotIn.size() == 2) { // 1 block diagonal perfect overlap
+                            run.accept(new TraverseReturn(v, false, pointID, exposedEdges)); // run consumer
+
+                            if (previous == null) previous = insideVertex.get(0);
+                            td = new TraverseData(BlockVector2.at(v.getX() + (v.getX() - previous.getX()), v.getZ() + (v.getZ() - previous.getZ())), v, false);
+                            cont = true;
+                        }
+                        // ignore if in middle of region (cornersNotIn size = 0)
+                        break;
+                }
+
+            }
+
+            // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ algorithm ends ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
             pointID++;
         }
@@ -74,72 +161,6 @@ public class RegionTraverse {
 
     // i need to find a faster way to do this
     // doesn't do so well with 1 block wide segments jutting out
-    private static void traverseRegionEdge(BlockVector2 v, BlockVector2 previous, BlockVector2 start, boolean first, HashSet<BlockVector2> points, List<ProtectedRegion> regions, int pointID, Consumer<TraverseReturn> run) {
-        if (!first && v.equals(start)) return;
-
-        int exposedEdges = 0;
-        List<BlockVector2> insideVertex = new ArrayList<>();
-        for (Vector2 dir : DIRECTIONS) {
-            BlockVector2 test = BlockVector2.at(v.getX() + dir.getX(), v.getZ() + dir.getZ());
-            if (!isInRegion(test, regions)) {
-                exposedEdges++;
-            } else {
-                insideVertex.add(test);
-            }
-        }
-        points.remove(v); // remove current point if it exists
-
-        switch (exposedEdges) {
-            case 1: // normal edge
-                run.accept(new TraverseReturn(v, false, pointID, exposedEdges)); // run consumer
-
-                if (previous == null) { // if this is the first node we need to determine a direction to go to (that isn't into the polygon, but is on edge)
-                    if (insideVertex.get(0).getX() == insideVertex.get(1).getZ() || insideVertex.get(0).getZ() == insideVertex.get(1).getZ() || insideVertex.get(0).getX() == insideVertex.get(2).getZ() || insideVertex.get(0).getZ() == insideVertex.get(2).getZ()) {
-                        previous = insideVertex.get(0);
-                    } else {
-                        previous = insideVertex.get(1);
-                    }
-                }
-                traverseRegionEdge(BlockVector2.at(v.getX() + (v.getX() - previous.getX()), v.getZ() + (v.getZ() - previous.getZ())), v, start, false, points, regions, pointID, run);
-                break;
-            case 2: // convex vertex
-                // possibly also 1 block wide segment with 2 edges opposite, but we'll ignore that
-                run.accept(new TraverseReturn(v, true, pointID, exposedEdges)); // run consumer
-                if (insideVertex.get(0).equals(previous)) {
-                    traverseRegionEdge(insideVertex.get(1), v, start, false, points, regions, pointID, run);
-                } else {
-                    traverseRegionEdge(insideVertex.get(0), v, start, false, points, regions, pointID, run);
-                }
-                break;
-            case 3: // random 1x1 jutting out
-                if (isInRegion(v, regions)) ProtectionStones.getInstance().getLogger().info("Reached impossible situation in region edge traversal at " + v.getX() + " " + v.getZ() + ", please notify the developers that you saw this message!");
-                break;
-            case 0: // concave vertex, or point in middle of region
-                List<Vector2> cornersNotIn = new ArrayList<>();
-                for (Vector2 dir : CORNER_DIRECTIONS) {
-                    BlockVector2 test = BlockVector2.at(v.getX() + dir.getX(), v.getZ() + dir.getZ());
-
-                    if (!isInRegion(test, regions)) cornersNotIn.add(dir);
-                }
-
-                if (cornersNotIn.size() == 1) { // concave vertex
-                    run.accept(new TraverseReturn(v, true, pointID, exposedEdges)); // run consumer
-
-                    Vector2 dir = cornersNotIn.get(0);
-                    if (previous == null || previous.equals(BlockVector2.at(v.getX() + dir.getX(), v.getZ()))) {
-                        traverseRegionEdge(BlockVector2.at(v.getX(), v.getZ() + dir.getZ()), v, start, false, points, regions, pointID, run);
-                    } else {
-                        traverseRegionEdge(BlockVector2.at(v.getX() + dir.getX(), v.getZ()), v, start, false, points, regions, pointID, run);
-                    }
-                } else if (cornersNotIn.size() == 2) { // 1 block diagonal perfect overlap
-                    run.accept(new TraverseReturn(v, false, pointID, exposedEdges)); // run consumer
-
-                    if (previous == null) previous = insideVertex.get(0);
-                    traverseRegionEdge(BlockVector2.at(v.getX() + (v.getX() - previous.getX()), v.getZ() + (v.getZ() - previous.getZ())), v, start, false, points, regions, pointID, run);
-                }
-                // ignore if in middle of region (cornersNotIn size = 0)
-                break;
-        }
-
-    }
+    //private static void traverseRegionEdge(BlockVector2 v, BlockVector2 previous, BlockVector2 start, boolean first, HashSet<BlockVector2> points, List<ProtectedRegion> regions, int pointID, Consumer<TraverseReturn> run) {
+    //}
 }
