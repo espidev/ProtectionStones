cdd0c298035c783145f5c487a704f04f76fa5690
Devin Lin <espidev@gmail.com>
2021-04-10 22:22:33 -0400

    Don't show farewell action if greeting action is going to be shown

--- src/main/java/dev/espi/protectionstones/flags/FarewellFlagHandler.java
+++ src/main/java/dev/espi/protectionstones/flags/FarewellFlagHandler.java
@@ -15,18 +15,23 @@
 
 package dev.espi.protectionstones.flags;
 
+import com.sk89q.worldedit.WorldEdit;
 import com.sk89q.worldedit.util.Location;
 import com.sk89q.worldguard.LocalPlayer;
 import com.sk89q.worldguard.protection.ApplicableRegionSet;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import com.sk89q.worldguard.session.MoveType;
 import com.sk89q.worldguard.session.Session;
 import com.sk89q.worldguard.session.handler.FlagValueChangeHandler;
 import com.sk89q.worldguard.session.handler.Handler;
 import dev.espi.protectionstones.FlagHandler;
+import dev.espi.protectionstones.PSRegion;
+import dev.espi.protectionstones.ProtectionStones;
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 
 // farewell-action flag
 public class FarewellFlagHandler extends FlagValueChangeHandler<String> {
@@ -48,16 +53,27 @@ public class FarewellFlagHandler extends FlagValueChangeHandler<String> {
     }
 
     @Override
-    protected boolean onSetValue(LocalPlayer localPlayer, Location location, Location location1, ApplicableRegionSet applicableRegionSet, String currentValue, String lastValue, MoveType moveType) {
-        if (lastValue != null && !lastValue.equals(currentValue) && Bukkit.getPlayer(localPlayer.getUniqueId()) != null)
-            Bukkit.getPlayer(localPlayer.getUniqueId()).spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(ChatColor.translateAlternateColorCodes('&', lastValue)));
+    protected boolean onSetValue(LocalPlayer localPlayer, Location from, Location to, ApplicableRegionSet toSet, String currentValue, String lastValue, MoveType moveType) {
+        Player p = Bukkit.getPlayer(localPlayer.getUniqueId());
+
+        // the greeting action flag for the other region should show instead if it is set
+        for (ProtectedRegion r : toSet.getRegions()) {
+            if (r.getFlag(FlagHandler.GREET_ACTION) != null) {
+                return true;
+            }
+        }
+        if (p != null && lastValue != null && !lastValue.equals(currentValue)) {
+            p.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(ChatColor.translateAlternateColorCodes('&', lastValue)));
+        }
         return true;
     }
 
     @Override
     protected boolean onAbsentValue(LocalPlayer localPlayer, Location location, Location location1, ApplicableRegionSet applicableRegionSet, String lastValue, MoveType moveType) {
-        if (lastValue != null && Bukkit.getPlayer(localPlayer.getUniqueId()) != null)
-            Bukkit.getPlayer(localPlayer.getUniqueId()).spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(ChatColor.translateAlternateColorCodes('&', lastValue)));
+        Player p = Bukkit.getPlayer(localPlayer.getUniqueId());
+        if (p != null && lastValue != null) {
+            p.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(ChatColor.translateAlternateColorCodes('&', lastValue)));
+        }
         return true;
     }
 }
